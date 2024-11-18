42220219ece9501234b1e44d939ccda59b126af5
assadov.spb <assadov.spb@bk.ru>
2024-11-18 18:41:56 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# Используем официальный образ Go как базовый для сборки
+FROM golang:1.23.3-alpine AS builder
+
+# Устанавливаем рабочую директорию внутри контейнера
+WORKDIR /app
+
+# Устанавливаем gcc и другие зависимости для сборки
+RUN apk add --no-cache gcc musl-dev
+
+# Копируем go.mod и go.sum для кэширования зависимостей
+COPY go.mod go.sum ./
+RUN go mod download
+
+# Копируем исходники приложения в рабочую директорию
+COPY . .
+
+# Собираем приложение
+RUN go build -o geo-service ./cmd/main.go
+
+# Начинаем новую стадию на основе минимального образа
+FROM alpine:latest
+
+# Копируем исполняемый файл из первой стадии
+COPY --from=builder /app/geo-service /geo-service
+
+# Копируем .env файл, если приложение использует его
+COPY .env /app/.env
+
+# Открываем порт для GRPC
+EXPOSE 30303
+
+# Запускаем приложение
+CMD ["./geo-service"]
---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	grpc2 "github.com/RVodassa/grpc_geoservice/internal/grpc-server"
+	"github.com/RVodassa/grpc_geoservice/internal/service"
+	pb "github.com/RVodassa/grpc_geoservice/proto/generated"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+	_ "google.golang.org/grpc/codes"
+	_ "google.golang.org/grpc/status"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	// Загружаем переменные окружения из .env файла
+	if err := godotenv.Load("/app/.env"); err != nil {
+		log.Println("Ошибка загрузки .env файла")
+	}
+
+	ApiKey, SecretKey := os.Getenv("apiKey"), os.Getenv("secretKey")
+
+	geoService := service.NewGeoService(ApiKey, SecretKey)
+
+	lis, err := net.Listen("tcp", ":30303")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGeoServiceServer(grpcServer, grpc2.NewServer(geoService))
+
+	log.Println("gRPC server is running on port ")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
